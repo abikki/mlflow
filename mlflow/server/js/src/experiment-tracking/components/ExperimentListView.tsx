ab3530c44c9b06d568ec0323065e3abe0049547c
abikki <ankaiah.bikki@oneconvergence.com>
2024-04-19 10:05:29 +0530

    type error fix

--- mlflow/server/js/src/experiment-tracking/components/ExperimentListView.tsx
+++ mlflow/server/js/src/experiment-tracking/components/ExperimentListView.tsx
@@ -40,9 +40,9 @@ type State = any;
 
 export class ExperimentListView extends Component<Props, State> {
   list: any;
-
+  selectedExperiments = localStorage.getItem('selected-experiments') || []
   state = {
-    checkedKeys: localStorage.getItem('selected-experiments') || this.props.activeExperimentIds,
+    checkedKeys: this.selectedExperiments ? this.selectedExperiments : this.props.activeExperimentIds,
     hidden: false,
     searchInput: '',
     project: localStorage.getItem('mlflow-exp-project') || 'All',
