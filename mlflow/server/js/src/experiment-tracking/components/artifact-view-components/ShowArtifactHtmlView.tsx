17ef6aebe2b06b048fc2b6991964e19f72bd8b97
Mahesh Krishna Tambe <mtambe@altoslabs.com>
2024-08-08 19:23:09 +0530

    allow-same-origin to allow HTML content in blob for html artifacts

--- mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactHtmlView.tsx
+++ mlflow/server/js/src/experiment-tracking/components/artifact-view-components/ShowArtifactHtmlView.tsx
@@ -70,7 +70,7 @@ class ShowArtifactHtmlView extends Component<ShowArtifactHtmlViewProps, ShowArti
             className="html-iframe"
             display="block"
             position="relative"
-            sandbox={"allow-scripts", "allow-same-origin"}
+            sandbox=({"allow-scripts","allow-same-origin"})
           />
         </div>
       );
